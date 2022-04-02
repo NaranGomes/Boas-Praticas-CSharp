d0f528fad2c14afce921004c1d9f9d5b4ddd0d66
Naran Gomes <gom3snaran@gmail.com>
2022-04-01 23:53:30 -0300

    Criada estrategia para modalidades de investimentos

---
+++ Strategy/Investimentos/Conta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investimentos
+{
+    public class Conta
+    {
+        public double Saldo { get; set; }
+        public void Deposita(double valor)
+        {
+            Saldo += valor;
+        }
+        
+    }
+}
