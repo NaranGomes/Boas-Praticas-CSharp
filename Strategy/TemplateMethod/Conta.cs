fda1d9ff86f1fe18c8fd84e9ae0c32c5f841e4b9
Naran Gomes <gom3snaran@gmail.com>
2022-05-28 16:22:33 -0300

    Projeto TemplateMethod Adicionado

---
+++ Strategy/TemplateMethod/Conta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    public class Conta
+    {
+        public string Nome { get; set; }
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+        public double Saldo { get; set; }
+
+    }
+}
